f72a109efe71821f5ec8573a15e85a25b7b543f3
Tiffsparks21 <143512641+Tiffsparks21@users.noreply.github.com>
2023-09-19 08:38:56 -0400

    Finished :)

--- Assignment 1/Debug.py
+++ Assignment 1/Debug.py
@@ -5,3 +5,62 @@ print(TB)
 print(TB)
 x=10
 print("my name is",TB,"and I am",x,"years old" )
+
+x=1
+y=2
+if y>x:
+    print (y)
+else:
+    print("is smaller than 3")
+
+toCheck = 0
+if toCheck > 1:
+    print(toCheck)
+elif toCheck == 0:
+    print(toCheck, "is equal 0")
+else:
+    print(toCheck, "is smaller than 3")
+
+
+#T = Tiffany
+#if T == "tiffany":
+ #   print ("wrong name")
+#elif T == "Tiffany":
+#    print( T, "is correct")
+#else:
+ #   print("ur not me")
+
+F=5
+S=6
+if F==S:
+    print("equal")
+else:
+    print("not equal")
+
+var = 4
+if var > 5:
+    print("too big")
+elif var > 0 and var < 5:
+    print("Just right!")
+else:
+    print("too small!")
+
+#age = input ("how old are you? ")
+#age = int(age)
+#      int("24")
+##age=24
+##age = 24
+#if age >= 21: #or if int(age) >= 21:
+   # print("You can drink!")
+#else:
+    #print ("it's illegal for you to drink :(")
+
+##5 and take 2 sets of 5. 1 is left over
+##moduals%= take out sets of two until you can't no more.
+
+num1 = input("enter a number to check if its even or odd")
+num1 = int(num1)
+if num1 % 2 == 0:
+    print("even number!")
+else:
+    print("odd number")
--- Assignment 2/Debug.py
+++ Assignment 2/Debug.py
@@ -9,40 +9,46 @@ def main():
 
     #this isn't printing anything :( so sad. what's wrong with her? Why she not printing out?
     color = input("what's your favorite color? ")
+    print(color," is a cool color :D !")
     
     #I thought i did this right :( can you fix it for me?
     age = input("how old are you? ")
-    print("You are " + age + " years old")
+    age = int(age)
+    print("You are " + str(age) + " years old")
 	
     #Start with this one! We have a compilation error :(
     #Side note: you can put these statements in parentheses or not. it's up to you.
-    if (5 > 3)
+    if (5 > 3):
         print("5 is greater than 3")
+    else:
+        print("Wrong")
     
     #There are multiple correct answers and adjustments to this one 
-    isFive = 5
-    if isFive > 5:
-        print("isFive is greater than 5")
+    Five = 5
+    if Five > 5:
+        print("Five is greater than 5")
+    elif Five == 5:
+        print("Five is equal to 5")
     else:
-        print("isFive is less than 5")
+        print("Five is less than 5")
 
     #more multiple correct answers. Similar to the first. Adjust as perceived 
     toCheck = 4
     if toCheck > 5:
         print(toCheck, "is greater than 5.")
-    elif toCheck < 3:
+    elif toCheck > 3:
 	    print(toCheck)
     else:
         print(toCheck, "is 3")
 
     #Is it really printing the BEST option though? Rearrange these as you see fit
     toCheck = 5
-    if toCheck < 6:
-        print("toCheck is less than 6")
+    if toCheck == 5:
+        print("toCheck is 5")
     elif toCheck > 7:
         print("toCheck is greater than 7")
-    elif toCheck == 5:
-        print("toCheck is 5")
+    elif toCheck < 6:
+        print("toCheck is less than 6")
     else:
         print("toCheck is invalid")
     
