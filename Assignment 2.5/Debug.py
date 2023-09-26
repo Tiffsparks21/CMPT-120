8e32229d24775936aebfb85ab35142d747d62629
Tiffsparks21 <143512641+Tiffsparks21@users.noreply.github.com>
2023-09-26 08:41:11 -0400

    Finished ;D

--- Assignment 2.5/Debug.py
+++ Assignment 2.5/Debug.py
@@ -21,7 +21,7 @@ def main():
     
     #we're gonna check if a user input number is even
     #pick any number
-    even = input("Enter a number to find out if its even or odd")
+    even = input("Enter a number to find out if its even or odd ")
     even = int(even)
     #what do we replace the question marks with?
     if even % 2 == 0:
@@ -33,21 +33,21 @@ def main():
   
     #i'm trying to do math with the numbers 2 and 4, but it's getting 3 and 5... why?
     numbers = [1,2,3,4,5]
-    print(numbers[2]-1)
-    print(numbers[4]-1)
-    print(numbers[4] / numbers[2])
-    print(numbers[4] * numbers[2])
+    print(numbers[1])
+    print(numbers[3])
+    print(numbers[3] / numbers[1])
+    print(numbers[3] * numbers[1])
     
     #again, why is it getting 7 and 20, I wanted 3 and 29!
-    numbers2 = [465,3,30,7,29,20,82,13,5]
-    print(numbers2[3])
-    print(numbers2[5])
+    numbers2 = [465,3,30,7,29,20,82,13,5] 
+    print(numbers2[1])
+    print(numbers2[4])
     
     #Here's a fun one: This is a list of everyone's name. Find where yours is and print the index of your name
     students = ["Tobi", "Brad", "Tiff", "Oscar", "Tommy", "Kyra", "Matt C", "Morgan", "Haley", "Matt F", "Sydney",
                 "Pedro", "Nathan", "Bryce", "Chris", "Iram", "Pat", "Maddie", "Daniel", "Tomas", "Gabriella", "Ben",
                 "Lucian", "Dean", "Jack", "Natalie", "Athina"]
-    print(students[3])
+    print(students[2])
 main()
     
     
