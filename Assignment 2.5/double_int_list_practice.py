572ef1d52f272b0d97abd8770f00a6a1627060e8
Tiffsparks21 <143512641+Tiffsparks21@users.noreply.github.com>
2023-09-26 18:05:02 -0400

    Finished :D

--- Assignment 2.5/double_int_list_practice.py
+++ Assignment 2.5/double_int_list_practice.py
@@ -1,29 +1,38 @@
 def main():
   
   #set this to any double
-  doubleValue =
+  doubleValue = 10.5
   
   #set this to any int
-  intValue = 
+  intValue = 20
   
   #print out addition, subtraction, multiplication, and division of these two values
-  
-  
+  print (doubleValue- intValue)
+  print(doubleValue + intValue)
+  print(doubleValue * intValue)
+  print(doubleValue / intValue)
+
   
   #populate this list
-  myFriends = []
+  myFriends = ["Nick", "Sebastian", "Alex", "Chalupa Arca", "Paul", "Noah", "Justin", "Kate"]
   
   #print out your friends at index 2 and index 3
-  
+  print (myFriends[2])
+  print (myFriends[3])
   
   #populate this list with five numbers
-  fiveNumbers = []
+  fiveNumbers = [21,22,18,23,17]
   
   #do each of the four equations with different numbers each time.
-  
+  print(fiveNumbers[0] - fiveNumbers[2])
+  print(fiveNumbers[1] + fiveNumbers[3])
+  print(fiveNumbers[4] * fiveNumbers[2])
+  print(fiveNumbers[1] / fiveNumbers[4])
+
   #now replace two of the numbers in the list with a different number (using name of list[x] = ?, not rewriting the fiveNumber list)
-  
+  fiveNumbers[1]=35
+  fiveNumbers[0] = 19
   #print out the list
-  
+  print(fiveNumbers)
   
 main()
--- Assignment 2.5/if_else_sign_practice.py
+++ Assignment 2.5/if_else_sign_practice.py
@@ -6,7 +6,16 @@ def main():
         If they're less than 18, they can only purchase candy cigarettes and sody pops
     '''
     
-  
+    age= input("what is your age? ")
+    age=int(age)
+    if age >= 21:
+        print ("You can buy alcohol,nicotine products, and they can rent a car")
+    elif age == 18 and age < 21:
+        print ("You can only buy nicotine products in some states")
+    else:
+        print("You can only purchase candy cigarettes and sody pops")
+
+
 
   
   
