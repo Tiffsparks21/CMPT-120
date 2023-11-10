752a3fb12fc6b303279ca2b2e2a0fe53fc755a45
Tiffsparks21 <143512641+Tiffsparks21@users.noreply.github.com>
2023-11-10 02:18:42 -0500

    Finished :D ananas

--- Assignment 6/Debug.py
+++ Assignment 6/Debug.py
@@ -10,6 +10,9 @@ class Employee:
         self.department = department
         
 class Cake:
+    def __init__(self, flavor, frosting):
+        self.flavor = flavor
+        self.frosting = frosting
     #can you fill out the rest of this for me? im dumb
     #the cake needs to have the cake flavor and cake frosting stored
 
@@ -31,37 +34,42 @@ class Car:
         self.model = model
         self.year = year
         self.color = color
-        
+
+    def getModel(self):
+        return (self.model)
     #create your own function! what do you want it to do?
     
    
 def main():
     #fill this one out with a dog's name and age.. can be your dog, friend's dog, etc
-    newDog = Dog(? , ?)
+    dog1 = Dog("Mellow", 9)
     print(dog1.name, dog1.age)
     
     #and what about a new employee
-    newEmployee =
+    newEmployee = Employee("Dave", 35, 120)
     #how would we print out each of the variables from newEmployee?
-    print()
+    print(newEmployee.name,newEmployee.department, newEmployee.idNumber)
     
     #now create and print out a cake you make
-    
-    
-    
+    cake1 = Cake("strawberry", "raspberry")
+    print(cake1.frosting, cake1.flavor)
+
+    cake2 = Cake("coco", "buttermilk")
+    print(cake2.frosting, cake2.flavor)
     #and now create another cake and print it out
     
     
     
     #create a cat!
-    cat1 = Cat()
+    cat1 = Cat("Mushi", 5, "long")
     #create another cat!
-    
+    cat2 = Cat("Max", 4, "short")
     #What would we put here to print out the result of breedGuess for cat1?
-    print(cat1.?)
+    print(cat1.breedGuess())
     
     #create a car!
-    
+    car1 = Car("Toyota", 2016, "red")
+    print(car1.getModel())
     #Now print out the function you made for car :)
 
 main()
