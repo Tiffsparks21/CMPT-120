089f9279cdce625af5603cd8e23da4a6c1ad73b9
Tiffsparks21 <143512641+Tiffsparks21@users.noreply.github.com>
2023-11-10 01:31:24 -0500

    banana

--- Assignment 4/blackjack.py
+++ Assignment 4/blackjack.py
@@ -2,21 +2,23 @@
 #this script is going to require some googling: I want you to practice using your resources with this one. But of course if you get stuck, reach out :)
 '''instructions: randomly generate three values between 1 and 11. in the function bust: add these three numbers together. if they add up to or less than 21, return the sum. If it's over 21, return 0. If it's over 21 BUT there's an 11 as one of the values, return the sum - 10. '''
 
-def bust(y,e,s):
-    addition(y+e+s)
+import random
+def bust(y, e, s):
+    x = y + e + s
     # do I make another def addition?
-    if addition <= 21:
-        return addition
-    elif addition > 21 and y or e or s  == 11:
+    if x <= 21:
+        return x
+    elif (x > 21) and ((y == 11) or (e == 11) or (s == 11)):
     #If it's over 21 BUT there's an 11 as one of the values, return the sum - 10
-        return addition-10
-    elif addition > 21:
-        return 0
+        return x-10
     else:
-        print ("pick a different number")
-    
+        return 0
+
 def main():
-    bust(3,5,11)
+    y = random.randint(1,11)
+    e = random.randint(1,11)
+    s = random.randint(1,11)
+    print(bust(y,e,s))
 
 
 main()
--- Notes.py
+++ Notes.py
@@ -1,42 +1,66 @@
-name="Tiffany"
-for x in range(5):
-    print(name)
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
-print("  ")
-name="Kate"
-animals= ["dog","cat","horse"]
-for x in animals:
-    print (name)
-print("")
-#loops going for the amount of letters in the name
-for x in "Marist":
-    print (x)
-print("  ")
+    def area(self):
+        return self.length * self.length
 
-name= "Alex"
-for x in name:
-    print(x)
+    def perimeter(self):
+        return 2 * self.length + 2 * self.width
 
-name= ["T","i","Ff","F","A","N","Y"]
-for x in name:
-    print(x)
 
-i=0
-while i<6:
-    print (i)
-    i=i+1#if I don't have this it will cause an infinite loop
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
 
-c=0
-while c<= 10:
-    print(c)
-    c=c+1
-# != not equal
-# <= less than or equal
-# >= more than or not equal
-# == equals
-# = assign to
-P = "Nick"
-x= 0
-while x < 5:
-    print(P)
-    x = x + 1
+
+def main():
+    square1 = Square(2)
+    print(square1.perimeter())
+
+
+main()
+
+# name="Tiffany"
+# for x in range(5):
+#     print(name)
+#
+# print("  ")
+# name="Kate"
+# animals= ["dog","cat","horse"]
+# for x in animals:
+#     print (name)
+# print("")
+# #loops going for the amount of letters in the name
+# for x in "Marist":
+#     print (x)
+# print("  ")
+#
+# name= "Alex"
+# for x in name:
+#     print(x)
+#
+# name= ["T","i","Ff","F","A","N","Y"]
+# for x in name:
+#     print(x)
+#
+# i=0
+# while i<6:
+#     print (i)
+#     i=i+1#if I don't have this it will cause an infinite loop
+#
+# c=0
+# while c<= 10:
+#     print(c)
+#     c=c+1
+# # != not equal
+# # <= less than or equal
+# # >= more than or not equal
+# # == equals
+# # = assign to
+# P = "Nick"
+# x= 0
+# while x < 5:
+#     print(P)
+#     x = x + 1
