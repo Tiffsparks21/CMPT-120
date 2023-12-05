e9f182c03c43cdaf32001bd9e53128b71f2495d1
Tiffsparks21 <143512641+Tiffsparks21@users.noreply.github.com>
2023-12-05 09:16:33 -0500

    Finish Project <3!!

--- Final Project 2.py
+++ Final Project 2.py
@@ -1,7 +1,7 @@
 #This is a choose your own adventure game!
 #QUESTIONS:
 
-
+FIN = "THE END"
 def YoshRoast():
     print("You walk downstairs and see Bowser at the head of the dinning table. You were ready to fight Bowser but he was \n"
           "looking down while leaning his head on this right hand. Bowser finally look up to meet your gaze. He said 'I'm\n"
@@ -9,7 +9,7 @@ def YoshRoast():
           "Dragon-Turtle Host and try to get to know the 'Real' Raspberry underneath that annoyance of hers, but she doesn't even\n"
           "say thank you after the (illegal) Roasted Yoshi I got for her dinner. You know what, just take her' \n")
     print("You happily took Raspberry and gave her to Wario. You got your (illegal) blue Yoshi and partied with Peach, Mario, and Luigi\n"
-          "while talking shit about Raspberry. THE END")
+          "while talking shit about Raspberry.", FIN)
 
 def lava():
     print("You headed towards the balcony and you see that Raspberry is tied up too a pole surrounded by the floor being lava.\n"
@@ -17,14 +17,21 @@ def lava():
           "face everytime you fell in the playground when you guys were kids. 'what a bitch' you thought... so you left. Leaving\n"
           "Raspberry to die and you went to the wedding. Wario was pissed but relaxed after thinking that you were doing him a \n"
           "favor to not his desperation get the best of him. But you still didn't get the Yoshi but that's ok cause you partied \n"
-          "with Peach, Mario, and Luigi on the dance floor. THE END...*Raspberry voice*-> 'Or is it?' \n")
+          "with Peach, Mario, and Luigi on the dance floor.", FIN, "...*Raspberry voice*-> 'Or is it?' \n")
 
 def roulette():
     print("You jump into the portrait and appeared at a small room. You see a 'Toy' gun on the table and \n"
           "a letter saying 'play russian roulette. spin the dial once and if you live I will give you Raspberry\n"
           "if you die, I get to give BOO your dead body as his wedding present. I'm always watching so don't\n"
           "try anything.\n")
-    spinNumber = int(input("How many times will you spin the dial? Pick a number between 1-6 "))
+    while True:#USE while loops to focus what is most important to you
+        spinNumber = input("How many times will you spin the dial? Pick a number between 1-6 ")
+        if spinNumber.isnumeric():
+            spinNumber = int(spinNumber)
+            break
+        else:
+            print("You input not a number. type in a number")
+
     while spinNumber <= 6:
         print(spinNumber)
         spinNumber = spinNumber - 1
@@ -35,11 +42,9 @@ def roulette():
                 " 'What took you so long? Even Luigi would have been here quicker than you'....You punched her in the face for her ungrateful ass\n"
                 "and carried the unconscous bitch over your shoulder to the wedding. Once you arrived you tossed her towards Wario and Raspberry woke up happy in Wario's arms.\n"
                 "You danced with Peach, Mario, and Luigi and told them the tea of how you heard Boswer's minions Gossiping on how Bowser was wearing earplugs to not ear \n"
-                "Raspberry bitching of not being the next princess during her stay. You got had fun at the party with your blue Yoshi. THE END\n")
+                "Raspberry bitching of not being the next princess during her stay. You got had fun at the party with your blue Yoshi.", FIN, "\n")
             break
-        else:
-            print("Please pick a number between 1-6")
-            pass
+      #  elif
         #return# Do I even need this???
 
 def main():
@@ -77,10 +82,15 @@ def main():
             # path # will it return to the start of the game? Will it loop around to the start until reached final ending?
 
 #1 for/ While Loops
+        #(Done)
 #3 variables
+    #Path, yourName, spinNumber
 #All INPUTS SANITIZED
+    #Path(done), yourName(done), spinNumber(done)
 #1 function
+    #(Done)?
 #Proper variable names
+    #Path, yourName, spinNumber
 #1 global variable, used in a function
 
 main()
